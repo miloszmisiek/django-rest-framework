98acc5c52f428ab028bd732ee6eb750c83d3c1ed
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-07-22 17:50:36 +0200

    Add ProfileList in views and serializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serialaziers import ProfileSerializer
 
-# Create your views here.
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serialaizer = ProfileSerializer(profiles, many=True)
+        return Response(serialaizer.data)
